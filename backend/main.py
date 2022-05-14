eba6cee9a36bbd5a3a5cfae1751f79d62a9ba7e7
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2022-05-14 14:52:03 +0700

    add cors

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from . import crud, models, schemas
+from . import models
 from .database import engine
 from .routers import todo, user, auth
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -11,3 +12,15 @@ app = FastAPI()
 app.include_router(user.router, prefix="/api")
 app.include_router(todo.router, prefix="/api")
 app.include_router(auth.router)
+
+origins = [
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
